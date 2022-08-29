153fd6a7100b6191ad38aec85b0c3af8a13ec835
BraisonCrece <braisoncrece@gmail.com>
2022-08-29 15:08:48 +0200

    first validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+    belongs_to :user
+
+    validates :title, :body, presence: { message: "musn't be blank"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+
+    validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true
 end
