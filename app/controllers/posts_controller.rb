d28021dd901e537d55c18de1e3e5a644c4510ca1
BraisonCrece <braisoncrece@gmail.com>
2022-08-29 14:08:26 +0200

    Blog v 1.0 ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  def new
-  end
+  before_action :set_post, only: %i[ show edit update destroy ]
 
-  def create
+  def index
+    @posts = Post.all
   end
 
   def show
@@ -11,9 +11,45 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.valid?
+      @post.save
+      redirect_to blog_index_path, notice: "Post successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def update
+    @post.user_id = current_user.id
+    if @post.update(post_params)
+      redirect_to blog_index_path, notice: "Post successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end 
   end
 
   def destroy
+    @post.destroy
+    redirect_to blog_index_path, notice: "Post successfully deleted"
   end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :registrations, only: %i[new create]
   resources :logins, only: %i[new create]
-  resources :posts, only: %i[new create show edit update destroy]
+  resources :posts, only: %i[index new create show edit update destroy]
   resources :users, only: %i[new create show edit update]
   resources :blog, only: %i[index]
   delete "logout", to: "sessions#destroy", as: :logout
